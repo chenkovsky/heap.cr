dcc008fb370bc6de99b574f20d08806c9ca362e4
chenkovsky <chenkov@yeah.net>
2016-11-14 00:03:39 +0800

    add min-max heap

--- README.md
+++ README.md
@@ -65,6 +65,32 @@ describe Heap do
     a.heapify.should eq([1, 2, 3])
   end
 end
+
+it "min_max_heap" do
+  heap = MinMaxHeap(Int32).new
+  heap << 1
+  heap << 90
+  heap << 100
+  STDERR.puts heap.to_a
+  arr = [1, 90, 100, 4, 8, 3, 2, 85, 40, 55, 70, 75, 60, 50, 10, 80]
+  heap = MinMaxHeap(Int32).new arr
+  heap.pop_last.should eq(100)
+  heap.pop_last.should eq(90)
+  heap << 100
+  heap << 90
+  arr2 = [] of Int32
+  while heap.size > 0
+    arr2 << (heap.pop)
+  end
+  arr2.should eq(arr.sort)
+  heap = MinMaxHeap.new arr, max_size: 4
+  arr2 = [] of Int32
+  while heap.size > 0
+    arr2 << (heap.pop)
+  end
+  arr2.should eq([1, 2, 3, 4])
+end
+
 ```
 
 
--- spec/heap_spec.cr
+++ spec/heap_spec.cr
@@ -21,7 +21,7 @@ describe Heap do
 
   it "mergeby" do
     res = [] of Int32
-    Array(Int32).merge_by([3, 2, 1], [6, 5, 4], key_func = ->(x : Int32) { -x }) do |x|
+    Array(Int32).merge_by([3, 2, 1], [6, 5, 4], key_func: ->(x : Int32) { -x }) do |x|
       res << x
     end
     res.should eq([6, 5, 4, 3, 2, 1])
@@ -44,5 +44,30 @@ describe Heap do
   it "heapify" do
     a = [3, 2, 1]
     a.heapify.should eq([1, 2, 3])
+  end
+
+  it "min_max_heap" do
+    heap = MinMaxHeap(Int32).new
+    heap << 1
+    heap << 90
+    heap << 100
+    STDERR.puts heap.to_a
+    arr = [1, 90, 100, 4, 8, 3, 2, 85, 40, 55, 70, 75, 60, 50, 10, 80]
+    heap = MinMaxHeap(Int32).new arr
+    heap.pop_last.should eq(100)
+    heap.pop_last.should eq(90)
+    heap << 100
+    heap << 90
+    arr2 = [] of Int32
+    while heap.size > 0
+      arr2 << (heap.pop)
+    end
+    arr2.should eq(arr.sort)
+    heap = MinMaxHeap.new arr, max_size: 4
+    arr2 = [] of Int32
+    while heap.size > 0
+      arr2 << (heap.pop)
+    end
+    arr2.should eq([1, 2, 3, 4])
   end
 end
